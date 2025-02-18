1a3d3c6ac17cf36b571fb170feb80b1d357b3e69
Karla Alaro <karlahera03@gmail.com>
2025-02-18 08:49:01 +0200

    Added an operate function for user to choose an operator

--- .js
+++ .js
@@ -10,8 +10,18 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 };
-
+function operate(operator, a, b){
+if(operator === "+"){
+    return add(a,b);
+} else if(operator === "-"){
+    return subtract(a,b);
+} else if(operator === "*"){
+    return multiply(a,b);
+} else if(operator === "/"){
+    return divide(a,b);
+}
+};
 const firstValue = Number(prompt("Enter the first Number"));
 const secondValue = Number(prompt("Enter the second Number"));
-const result = add(firstValue, secondValue);
-alert(result);
+const operator = prompt("Choose an operator(+, -, *, /)");
+alert(operate(operator, firstValue, secondValue));
