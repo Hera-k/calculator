6043dd98526b5fbc78648cb75048e7113d78ca04
Karla Alaro <karlahera03@gmail.com>
2025-02-18 12:51:14 +0200

    Styled the elements and created functions to do the operations, also added functionality to the buttons

--- .js
+++ .js
@@ -15,13 +15,48 @@ if(operator === "+"){
     return add(a,b);
 } else if(operator === "-"){
     return subtract(a,b);
-} else if(operator === "*"){
+} else if(operator === "x"){
     return multiply(a,b);
-} else if(operator === "/"){
+} else if(operator === "➗"){
     return divide(a,b);
 }
 };
-const firstValue = Number(prompt("Enter the first Number"));
-const secondValue = Number(prompt("Enter the second Number"));
-const operator = prompt("Choose an operator(+, -, *, /)");
-alert(operate(operator, firstValue, secondValue));
+let firstValue = "";
+let secondValue = "";
+let operatorString = "";
+//alert(operate(operator, firstValue, secondValue));
+
+const buttons= document.querySelectorAll(".number");
+const display = document.getElementById("display");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;;
+    });
+});
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        display.textContent += operator.textContent;
+    });
+})
+const equalsoperator = document.querySelector(".equalsoperator");
+equalsoperator.addEventListener("click", () => {
+    const equationSplit = display.textContent.split("");
+    firstValue = Number(equationSplit[0]);
+    operatorString = equationSplit[1];
+    secondValue = Number(equationSplit[2]);
+    display.textContent+= "=";
+    display.textContent += operate(operatorString, firstValue, secondValue);
+});
+const clearButton = document.querySelector(".clearButton");
+clearButton.addEventListener("click", () => {
+    display.textContent = "";
+});
+
+const ACbutton = document.querySelector(".ACbutton");
+ACbutton.addEventListener("click", () => {
+    let spliteq = display.textContent.split("");
+    const len = display.textContent.split("").length;
+    spliteq.splice(-1, 1);
+    display.textContent = spliteq.join("");
+});
